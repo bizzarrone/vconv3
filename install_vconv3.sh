e79bae43bfafd2740c31bd3d8a6e3d952ff817a7
Luca <bizzarrone@gmail.com>
2017-10-27 16:52:29 +0200

    aggiornato installer

--- install_vconv3.sh
+++ install_vconv3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # versione script installazione autoatica: 2.0
 # Autore: Luca C.
-# Ultima modivica: 17/5/2017
+# Ultima modivica: 27/10/2017
 
 # aggiorno il sistema
 apt-get update
-apt-get -y install vim screen aptitude samba git  dfc nmon libssl-dev openssh-server  libav-tools  libavcodec54  apache2 php5 make openssh-server
+apt-get -y install vim screen aptitude samba git dfc nmon libssl-dev openssh-server  libav-tools libavcodec-ffmpeg56 apache2 php5 make openssh-server
 apt-get upgrade -y
 locale-gen en_US en_US.UTF-8 it_IT
 dpkg-reconfigure locales
@@ -27,10 +27,9 @@ cd
 # preparo il programma
 #mkdir /scripts
 cd /
-git clone https://github.com/bizzarrone/vconv2.git
-#mv /scripts/vconv/avconvluca.sh /scripts/
-touch /vconv2/vconv2.log
-chmod 777 /vconv2/parametri.txt
+git clone https://github.com/bizzarrone/vconv3.git
+touch /vconv3/vconv3.log
+chmod 777 /vconv3/parametri.txt
 
 # CREAZIONE Share di rete
 mkdir /CONDIVISA
@@ -49,20 +48,20 @@ echo  " writable = yes" >> /etc/samba/smb.conf
 echo  " guest ok = yes" >> /etc/samba/smb.conf
 
 # creare servizio all'avvio
-mv /vconv2/edge  /etc/init.d/
-mv /vconv2/vconv2 /etc/init.d/
+mv /vconv3/edge  /etc/init.d/
+mv /vconv3/vconv3 /etc/init.d/
 update-rc.d edge defaults
 update-rc.d edge enable
-update-rc.d vconv2 defaults
-update-rc.d vconv2 enable
-yyupdate-rc.d apache2 defaults
-yyupdate-rc.d apache2 enable 
+update-rc.d vconv3 defaults
+update-rc.d vconv3 enable
+update-rc.d apache2 defaults
+update-rc.d apache2 enable 
 
 # installare le webpages
 cd /
-wget https://github.com/bizzarrone/vconv2/raw/master/vconv2web.tar.gz
-cd / ; tar xvzf vconv2web.tar.gz
-rm /vconv2web.tar.gz
+wget https://github.com/bizzarrone/vconv3/raw/master/vconv3web.tar.gz
+cd / ; tar xvzf vconv3web.tar.gz
+rm /vconv3web.tar.gz
 chown -R www-data:www-data  /var/www/html
 rm /var/www/html/index.html
 reboot
